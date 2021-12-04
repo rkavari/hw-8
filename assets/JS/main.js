f54846a34e9674cb4967487927496519fa12f62d
rkavari <74878950+rkavari@users.noreply.github.com>
2021-12-04 01:12:53 -0500

    Update main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,7 @@
+/* Image animation
+  Resource/code referenced while creating animation for image 
+    https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API */
+
 function funcAnimate(){
 const element = document.getElementById('animateid');
 element.animate({ transform: 'scale(0)', opacity: 0 }, 300);
